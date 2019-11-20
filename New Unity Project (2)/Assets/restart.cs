f84b2a2cdc0693cd1c1cf876d0980408cc7c4a2f
Joshu Groves <joshgroves@outlook.com>
2019-11-20 14:21:38 +1300

    Test comment

---
+++ New Unity Project (2)/Assets/restart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class restart : MonoBehaviour {
+  void OnGUI () {
+    if (GUILayout.Button ("R = Restart")) {
+      Application.LoadLevel(Application.loadedLevel);
+    }
+    if (GUI.Button (new Rect (10,30,75,20), "Restart")) {
+      Application.LoadLevel(Application.loadedLevel);
+    }
+  }
+
+  void Update () {
+    if (Input.GetKey ("r")) {
+    Application.LoadLevel(Application.loadedLevel);
+    }
+    //Requires this button to be set in the Input Manager
+    if (Input.GetButtonDown ("Restart")) {
+      Application.LoadLevel(Application.loadedLevel);
+    }
+  }
+}
